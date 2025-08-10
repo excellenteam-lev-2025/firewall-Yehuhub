6aa8c3145a8f6c747f4465c0fbc918a1dbef84e1
Yehuhub <yhrcch@gmail.com>
2025-08-10 12:44:17 +0300

    cleared main local

--- src/db/DbSetup.ts
+++
@@ -1,6 +0,0 @@
-const dbURL = "postgres://admin:Aa123456@localhost:5432/firewalldb"; //this needs to be in a .env file
-import { Sequelize } from "sequelize";
-
-const sequelize = new Sequelize(dbURL, { dialect: "postgres" });
-
-export default sequelize;
